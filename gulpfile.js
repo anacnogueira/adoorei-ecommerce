934e63b8cd5b4f2e2678503a626b3f7b3ce33a26
Ana Claudia <anacnogueira@gmail.com>
2023-03-22 10:47:17 -0300

    Adiciona tarefa de SASS

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,20 @@ const gulp = require("gulp");
 
 const { series, parallel, dest } = require("gulp");
 
+const sass = require("gulp-sass")(require("sass"));
+const sourcemaps = require("gulp-sourcemaps");
+const autoprefixer = require("gulp-autoprefixer");
+const cssnano = require("gulp-cssnano");
+const rename = require("gulp-rename");
+const plumber = require("gulp-plumber");
+const notifier = require('gulp-notifier');
 const browserSync = require('browser-sync').create();
 const htmlmin = require("gulp-htmlmin");
 const kit = require("gulp-kit");
 
 filesPath = {
 	html: "./*.kit",
+	sass: "./styles/sass/*.scss",
 }
 
 function kitTask(done) {
@@ -20,6 +28,24 @@ function kitTask(done) {
 	done();
 }
 
+function sassTask(done) {
+	gulp
+		.src(filesPath.sass)
+		.pipe(plumber({ errorHandler: notifier.error }))
+		.pipe(sourcemaps.init())
+		.pipe(autoprefixer())
+		.pipe(sass())
+		.pipe(cssnano())
+	 	.pipe(sourcemaps.write("."))	 	
+	 	.pipe(rename(function(path){
+	 		if(!path.extname.endsWith(".map")) {
+	 			path.basename += ".min";
+	 		}	 		
+	 	}))
+		.pipe(dest("./dist/styles"))
+	done();
+}
+
 function watch() {
 	browserSync.init({
 		server: {
@@ -28,7 +54,15 @@ function watch() {
 	});
 
 	gulp.watch(filesPath.html, kitTask).on("change", browserSync.reload);
-}	
+	gulp.watch(filesPath.sass, sassTask).on("change", browserSync.reload);
+}
 
 exports.kitTask = kitTask;
+exports.sassTask = sassTask;
 exports.watch = watch;
+
+//Gulp Serve
+exports.build = parallel(sassTask, kitTask);
+
+//Gulp Default command
+exports.default = series(exports.build, watch);
